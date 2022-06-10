e9238d73db504e2d8f2d5b67674ef9dd8f99541d
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2022-06-10 09:47:53 -0300

    Revert "Merge branch 'main' into DPE-422-multiple-databases"
    This reverts commit 649ffde3cfc2082652fce3073deb01df55779b57, reversing
    changes made to fc4c0f9241701bff495a676e74acc5dcb7ee1ca5.

--- tests/integration/application-charm/src/charm.py
+++ tests/integration/application-charm/src/charm.py
@@ -34,55 +34,27 @@ class ApplicationCharm(CharmBase):
         # Default charm events.
         self.framework.observe(self.on.start, self._on_start)
 
-        # First database events (defined in the database requires charm library).
-        database_name = f'{self.app.name.replace("-", "_")}_first_database'
-        self.first_database = DatabaseRequires(
-            self, "first-database", database_name, EXTRA_USER_ROLES
-        )
-        self.framework.observe(
-            self.first_database.on.database_created, self._on_first_database_created
-        )
-        self.framework.observe(
-            self.first_database.on.endpoints_changed, self._on_first_database_endpoints_changed
-        )
-
-        # Second database events (defined in the database requires charm library).
-        database_name = f'{self.app.name.replace("-", "_")}_second_database'
-        self.second_database = DatabaseRequires(
-            self, "second-database", database_name, EXTRA_USER_ROLES
-        )
-        self.framework.observe(
-            self.second_database.on.database_created, self._on_second_database_created
-        )
-        self.framework.observe(
-            self.second_database.on.endpoints_changed, self._on_second_database_endpoints_changed
-        )
+        # Name of the database that this application requests to the database charm.
+        database = f'data_platform_{self.app.name.replace("-", "_")}'
+        # Charm events defined in the database requires charm library.
+        self.database = DatabaseRequires(self, "database", database, EXTRA_USER_ROLES)
+        self.framework.observe(self.database.on.database_created, self._on_database_created)
+        self.framework.observe(self.database.on.endpoints_changed, self._on_endpoints_changed)
 
     def _on_start(self, _) -> None:
         """Only sets an Active status."""
         self.unit.status = ActiveStatus()
 
     # First database events.
-    def _on_first_database_created(self, event: DatabaseCreatedEvent) -> None:
+    def _on_database_created(self, event: DatabaseCreatedEvent) -> None:
         """Event triggered when a database was created for this application."""
         # Retrieve the credentials using the charm library.
-        logger.info(f"first database credentials: {event.username} {event.password}")
-        self.unit.status = ActiveStatus("received database credentials for first database")
+        logger.info(f"_on_database_created called: {event.username} {event.password}")
+        self.unit.status = ActiveStatus("received database credentials")
 
-    def _on_first_database_endpoints_changed(self, event: DatabaseEndpointsChangedEvent) -> None:
+    def _on_endpoints_changed(self, event: DatabaseEndpointsChangedEvent) -> None:
         """Event triggered when the read/write endpoints of the database change."""
-        logger.info(f"first database endpoints have been changed to: {event.endpoints}")
-
-    # Second database events.
-    def _on_second_database_created(self, event: DatabaseCreatedEvent) -> None:
-        """Event triggered when a database was created for this application."""
-        # Retrieve the credentials using the charm library.
-        logger.info(f"second database credentials: {event.username} {event.password}")
-        self.unit.status = ActiveStatus("received database credentials for second database")
-
-    def _on_second_database_endpoints_changed(self, event: DatabaseEndpointsChangedEvent) -> None:
-        """Event triggered when the read/write endpoints of the database change."""
-        logger.info(f"second database endpoints have been changed to: {event.endpoints}")
+        logger.info(f"_on_endpoints_changed called: {event.endpoints}")
 
 
 if __name__ == "__main__":
