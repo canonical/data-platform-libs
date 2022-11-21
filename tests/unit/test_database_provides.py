915e3806673ae2324887f399846a4cf9cae76e79
Paolo Sottovia <paolo.sottovia@canonical.com>
2022-11-21 17:28:40 +0000

    Fix unit tests

--- tests/unit/test_database_provides.py
+++ tests/unit/test_database_provides.py
@@ -39,11 +39,11 @@ class DatabaseCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.database = DatabaseProvides(
+        self.provider = DatabaseProvides(
             self,
             DATABASE_RELATION_NAME,
         )
-        self.framework.observe(self.database.on.database_requested, self._on_database_requested)
+        self.framework.observe(self.provider.on.database_requested, self._on_database_requested)
 
     def _on_database_requested(self, _) -> None:
         pass
@@ -54,11 +54,11 @@ class KafkaCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.kafka = KafkaProvides(
+        self.provider = KafkaProvides(
             self,
             KAFKA_RELATION_NAME,
         )
-        self.framework.observe(self.kafka.on.database_requested, self._on_topic_requested)
+        self.framework.observe(self.provider.on.topic_requested, self._on_topic_requested)
 
     def _on_topic_requested(self, _) -> None:
         pass
@@ -68,7 +68,7 @@ class TestDataProvides(unittest.TestCase):
     charm = DatabaseCharm
     metadata = DATABASE_METADATA
     relation_name = DATABASE_RELATION_NAME
-    app = "database"
+    app_name = "database"
 
     def setUp(self):
         self.harness = Harness(self.charm, meta=self.metadata)
@@ -85,7 +85,7 @@ class TestDataProvides(unittest.TestCase):
         # Define a mock relation changed event to be used in the subsequent diff calls.
         mock_event = Mock()
         # Set the app, id and the initial data for the relation.
-        mock_event.app = self.harness.charm.model.get_app(self.app)
+        mock_event.app = self.harness.charm.model.get_app(self.app_name)
         mock_event.relation.id = self.rel_id
         mock_event.relation.data = {
             mock_event.app: {"username": "test-username", "password": "test-password"}
@@ -94,43 +94,45 @@ class TestDataProvides(unittest.TestCase):
         data = mock_event.relation.data[mock_event.app]
 
         # Test with new data added to the relation databag.
-        result = self.harness.charm.database._diff(mock_event)
+        result = self.harness.charm.provider._diff(mock_event)
         assert result == Diff({"username", "password"}, set(), set())
 
         # Test with the same data.
-        result = self.harness.charm.database._diff(mock_event)
+        result = self.harness.charm.provider._diff(mock_event)
         assert result == Diff(set(), set(), set())
 
         # Test with changed data.
         data["username"] = "test-username-1"
-        result = self.harness.charm.database._diff(mock_event)
+        result = self.harness.charm.provider._diff(mock_event)
         assert result == Diff(set(), {"username"}, set())
 
         # Test with deleted data.
         del data["username"]
         del data["password"]
-        result = self.harness.charm.database._diff(mock_event)
+        result = self.harness.charm.provider._diff(mock_event)
         assert result == Diff(set(), set(), {"username", "password"})
 
+    def test_set_credentials(self):
+        """Asserts that the database name is in the relation databag when it's requested."""
+        # Set the credentials in the relation using the provides charm library.
+        self.harness.charm.provider.set_credentials(self.rel_id, "test-username", "test-password")
 
-class TestDatabaseProvides(unittest.TestCase):
+        # Check that the credentials are present in the relation.
+        assert self.harness.get_relation_data(self.rel_id, self.app_name) == {
+            "data": "{}",  # Data is the diff stored between multiple relation changed events.
+            "username": "test-username",
+            "password": "test-password",
+        }
 
-    # def setUp(self):
-    #     self.harness = Harness(DatabaseCharm, meta=DATABASE_METADATA)
-    #     self.addCleanup(self.harness.cleanup)
 
-    #     # Set up the initial relation and hooks.
-    #     self.rel_id = self.harness.add_relation(DATABASE_RELATION_NAME, "application")
-    #     self.harness.add_relation_unit(self.rel_id, "application/0")
-    #     self.harness.set_leader(True)
-    #     self.harness.begin_with_initial_hooks()
+class TestDatabaseProvides(TestDataProvides):
 
     @patch.object(DatabaseCharm, "_on_database_requested")
     def emit_database_requested_event(self, _on_database_requested):
         # Emit the database requested event.
         relation = self.harness.charm.model.get_relation(DATABASE_RELATION_NAME, self.rel_id)
         application = self.harness.charm.model.get_app("database")
-        self.harness.charm.database.on.database_requested.emit(relation, application)
+        self.harness.charm.provider.on.database_requested.emit(relation, application)
         return _on_database_requested.call_args[0][0]
 
     @patch.object(DatabaseCharm, "_on_database_requested")
@@ -152,22 +154,10 @@ class TestDatabaseProvides(unittest.TestCase):
         assert event.database == DATABASE
         assert event.extra_user_roles == EXTRA_USER_ROLES
 
-    def test_set_credentials(self):
-        """Asserts that the database name is in the relation databag when it's requested."""
-        # Set the credentials in the relation using the provides charm library.
-        self.harness.charm.database.set_credentials(self.rel_id, "test-username", "test-password")
-
-        # Check that the credentials are present in the relation.
-        assert self.harness.get_relation_data(self.rel_id, "database") == {
-            "data": "{}",  # Data is the diff stored between multiple relation changed events.
-            "username": "test-username",
-            "password": "test-password",
-        }
-
     def test_set_endpoints(self):
         """Asserts that the endpoints are in the relation databag when they change."""
         # Set the endpoints in the relation using the provides charm library.
-        self.harness.charm.database.set_endpoints(self.rel_id, "host1:port,host2:port")
+        self.harness.charm.provider.set_endpoints(self.rel_id, "host1:port,host2:port")
 
         # Check that the endpoints are present in the relation.
         assert (
@@ -178,7 +168,7 @@ class TestDatabaseProvides(unittest.TestCase):
     def test_set_read_only_endpoints(self):
         """Asserts that the read only endpoints are in the relation databag when they change."""
         # Set the endpoints in the relation using the provides charm library.
-        self.harness.charm.database.set_read_only_endpoints(self.rel_id, "host1:port,host2:port")
+        self.harness.charm.provider.set_read_only_endpoints(self.rel_id, "host1:port,host2:port")
 
         # Check that the endpoints are present in the relation.
         assert (
@@ -189,11 +179,11 @@ class TestDatabaseProvides(unittest.TestCase):
     def test_set_additional_fields(self):
         """Asserts that the additional fields are in the relation databag when they are set."""
         # Set the additional fields in the relation using the provides charm library.
-        self.harness.charm.database.set_replset(self.rel_id, "rs0")
-        self.harness.charm.database.set_tls(self.rel_id, "True")
-        self.harness.charm.database.set_tls_ca(self.rel_id, "Canonical")
-        self.harness.charm.database.set_uris(self.rel_id, "host1:port,host2:port")
-        self.harness.charm.database.set_version(self.rel_id, "1.0")
+        self.harness.charm.provider.set_replset(self.rel_id, "rs0")
+        self.harness.charm.provider.set_tls(self.rel_id, "True")
+        self.harness.charm.provider.set_tls_ca(self.rel_id, "Canonical")
+        self.harness.charm.provider.set_uris(self.rel_id, "host1:port,host2:port")
+        self.harness.charm.provider.set_version(self.rel_id, "1.0")
 
         # Check that the additional fields are present in the relation.
         assert self.harness.get_relation_data(self.rel_id, "database") == {
@@ -211,7 +201,7 @@ class TestDatabaseProvides(unittest.TestCase):
 
         # Check the data using the charm library function
         # (the diff/data key should not be present).
-        data = self.harness.charm.database.fetch_relation_data()
+        data = self.harness.charm.provider.fetch_relation_data()
         assert data == {self.rel_id: {"database": DATABASE}}
 
     def test_database_requested_event(self):
