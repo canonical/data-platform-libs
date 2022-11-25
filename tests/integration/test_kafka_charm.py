0eafecb1a1081582620cff2ac261db65781ef115
Paolo Sottovia <paolo.sottovia@canonical.com>
2022-11-25 11:58:29 +0000

    Fix ntegration tests

--- tests/integration/test_kafka_charm.py
+++ tests/integration/test_kafka_charm.py
@@ -11,7 +11,7 @@ from tests.integration.helpers import get_application_relation_data
 
 logger = logging.getLogger(__name__)
 
-APPLICATION_APP_NAME = "app-2"
+APPLICATION_APP_NAME = "requirer-app"
 KAFKA_APP_NAME = "kafka"
 APP_NAMES = [APPLICATION_APP_NAME, KAFKA_APP_NAME]
 RELATION_NAME = "kafka_client"
@@ -124,7 +124,7 @@ async def test_kafka_credential_changed(ops_test: OpsTest):
     await ops_test.model.wait_for_idle(apps=APP_NAMES, status="active")
     assert result.results["username"] == "new-username"
 
-    # check that the new password is in the databag
+    # check that the new username is in the databag
     username = await get_application_relation_data(
         ops_test, APPLICATION_APP_NAME, RELATION_NAME, "username"
     )
@@ -146,7 +146,7 @@ async def test_kafka_credential_changed(ops_test: OpsTest):
     await ops_test.model.wait_for_idle(apps=APP_NAMES, status="active")
     assert result.results["username"] == "new-username"
 
-    # check that the new password is in the databag
+    # check that the new username is in the databag
     username = await get_application_relation_data(
         ops_test, APPLICATION_APP_NAME, RELATION_NAME, "username"
     )
@@ -157,60 +157,40 @@ async def test_kafka_credential_changed(ops_test: OpsTest):
 
 
 async def test_kafka_bootstrap_server_changed(ops_test: OpsTest):
-    pass
 
+    app_unit = ops_test.model.applications[APPLICATION_APP_NAME].units[0]
+    kafka_unit = ops_test.model.applications[KAFKA_APP_NAME].units[0]
+    # set new bootstrap
+    parameters = {"bootstrap-server": "host1:port,host2:port,host3:port"}
+    action = await kafka_unit.run_action(action_name="sync-bootstrap-server", **parameters)
+    result = await action.wait()
+    assert result.results["bootstrap-server"] == "host1:port,host2:port,host3:port"
+    # check that the new bootstrap-server is in the databag
+    bootstrap_server = await get_application_relation_data(
+        ops_test, APPLICATION_APP_NAME, RELATION_NAME, "endpoints"
+    )
+    assert bootstrap_server == "host1:port,host2:port,host3:port"
+    await ops_test.model.wait_for_idle(apps=APP_NAMES, status="active")
 
-# async def test_two_applications_doesnt_share_the_same_relation_data(
-#     ops_test: OpsTest, application_s3_charm
-# ):
-#     """Test that two applications connect to the s3 provider with different credentials."""
-#     # Set some variables to use in this test.
-#     another_application_app_name = "another-app"
-#     all_app_names = [another_application_app_name]
-#     all_app_names.extend(APP_NAMES)
-
-#     # Deploy another application.
-#     await ops_test.model.deploy(
-#         application_s3_charm,
-#         application_name=another_application_app_name,
-#     )
-#     await ops_test.model.wait_for_idle(apps=[S3_APP_NAME, APPLICATION_APP_NAME], status="active")
-#     await ops_test.model.wait_for_idle(apps=[another_application_app_name], status="waiting")
-#     # Relate the new application with the s3 provider
-#     # and wait for them exchanging some connection data.
-#     await ops_test.model.add_relation(
-#         f"{another_application_app_name}:{FIRST_S3_RELATION_NAME}", S3_APP_NAME
-#     )
-#     await ops_test.model.wait_for_idle(apps=all_app_names, status="active")
-
-#     # Assert the two applications have different relation (connection) data.
-#     application_connection_info = await get_connection_info(
-#         ops_test, APPLICATION_APP_NAME, FIRST_S3_RELATION_NAME
-#     )
-#     another_application_connection_info = await get_connection_info(
-#         ops_test, another_application_app_name, FIRST_S3_RELATION_NAME
-#     )
-
-#     assert application_connection_info != another_application_connection_info
-
-
-# async def test_an_application_can_request_multiple_s3_providers(ops_test: OpsTest):
-#     """Test that an application can request additional s3
-# credentials using the same interface."""
-#     # Relate the charms using another relation and wait for
-#  them exchanging some connection data.
-#     await ops_test.model.add_relation(
-#         f"{APPLICATION_APP_NAME}:{SECOND_S3_RELATION_NAME}", S3_APP_NAME
-#     )
-#     await ops_test.model.wait_for_idle(apps=APP_NAMES, status="active")
-
-#     # Get the connection infos from the two different relations.
-#     first_s3_connection_info = await get_connection_info(
-#         ops_test, APPLICATION_APP_NAME, FIRST_S3_RELATION_NAME
-#     )
-#     second_s3_connection_info = await get_connection_info(
-#         ops_test, APPLICATION_APP_NAME, SECOND_S3_RELATION_NAME
-#     )
-
-#     # Assert the two applications have different relation (connection) data.
-#     assert first_s3_connection_info != second_s3_connection_info
+    # check that the bootstrap_server_changed event is triggered
+    for unit in ops_test.model.applications[APPLICATION_APP_NAME].units:
+        assert unit.workload_status_message == "kafka_bootstrap_server_changed"
+    # reset unit message
+    action = await app_unit.run_action(action_name="reset-unit-status")
+    result = await action.wait()
+    await ops_test.model.wait_for_idle(apps=APP_NAMES, status="active")
+    # check if the message is empty
+    for unit in ops_test.model.applications[APPLICATION_APP_NAME].units:
+        assert unit.workload_status_message == ""
+    # configure the same bootstrap-server
+    action = await kafka_unit.run_action(action_name="sync-bootstrap-server", **parameters)
+    result = await action.wait()
+    await ops_test.model.wait_for_idle(apps=APP_NAMES, status="active")
+    assert result.results["bootstrap-server"] == "host1:port,host2:port,host3:port"
+    bootstrap_server = await get_application_relation_data(
+        ops_test, APPLICATION_APP_NAME, RELATION_NAME, "endpoints"
+    )
+    assert bootstrap_server == "host1:port,host2:port,host3:port"
+    # check the bootstrap_server_changed event is NOT triggered
+    for unit in ops_test.model.applications[APPLICATION_APP_NAME].units:
+        assert unit.workload_status_message == ""
