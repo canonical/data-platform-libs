272cbec4e5110882c99b580ae88909714118cc8d
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2023-07-10 16:10:42 +0100

    fix: use correct on's

--- lib/charms/data_platform_libs/v0/upgrade.py
+++ lib/charms/data_platform_libs/v0/upgrade.py
@@ -439,11 +439,11 @@ class DataUpgrade(Object, ABC):
             self.charm.on[relation_name].relation_changed, self.on_upgrade_changed
         )
         self.framework.observe(self.charm.on.upgrade_charm, self._on_upgrade_charm)
-        self.framework.observe(getattr(self.charm.on, "upgrade_granted"), self._on_upgrade_granted)
+        self.framework.observe(getattr(self.on, "upgrade_granted"), self._on_upgrade_granted)
 
         # actions
         self.framework.observe(
-            getattr(self.on, "pre_upgrade_check_action"), self._on_pre_upgrade_check_action
+            getattr(self.charm.on, "pre_upgrade_check_action"), self._on_pre_upgrade_check_action
         )
 
     @property
