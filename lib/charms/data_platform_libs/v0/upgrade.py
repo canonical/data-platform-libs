e095770f3e69362f62f9cfb8ea7f914e2a34bfab
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2023-07-11 17:16:12 +0100

    chore: bump lib ver

--- lib/charms/data_platform_libs/v0/upgrade.py
+++ lib/charms/data_platform_libs/v0/upgrade.py
@@ -39,7 +39,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 3
+LIBPATCH = 4
 
 PYDEPS = ["pydantic>=1.10,<2"]
 
