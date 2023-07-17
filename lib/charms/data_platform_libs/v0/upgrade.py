207875c1385315776e344f9ba6bad2e95c2c99cd
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2023-07-17 13:29:11 +0100

    chore: exit on idle|completed

--- lib/charms/data_platform_libs/v0/upgrade.py
+++ lib/charms/data_platform_libs/v0/upgrade.py
@@ -732,12 +732,12 @@ class DataUpgrade(Object, ABC):
 
         # if all units completed, mark as complete
         if not self.upgrade_stack:
-            if self.cluster_state == "idle":
-                logger.debug("upgrade-changed event handled before pre-checks, exiting...")
-                return
-            elif self.cluster_state == "completed":
+            if self.state == "completed" and self.cluster_state in ["idle", "completed"]:
                 logger.info("All units completed upgrade, setting idle upgrade state...")
                 self.peer_relation.data[self.charm.unit].update({"state": "idle"})
+                return
+            if self.cluster_state == "idle":
+                logger.debug("upgrade-changed event handled before pre-checks, exiting...")
                 return
             else:
                 logger.debug("Did not find upgrade-stack or completed cluster state, deferring...")
