0cc355b0ffdc068a1e473cbb882c92bb4f67b956
Judit Novak <judit.novak@canonical.com>
2023-08-30 19:19:46 +0200

    Charms in a new location

---
+++ tests/integration/charms/application-charm/src/charm.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Application charm that connects to database charms.
+
+This charm is meant to be used only for testing
+of the libraries in this repository.
+"""
+
+import logging
+from importlib import import_module
+
+from ops import JujuVersion, EventBase
+from ops.charm import ActionEvent, CharmBase
+from ops.main import main
+from ops.model import ActiveStatus
+
+try:
+    df_module = import_module("charms.data_platform_libs.v0.data_interfaces")
+except ImportError:
+    df_module = import_module("charms.data_platform_libs.v1.data_interfaces")
+
+
+BootstrapServerChangedEvent = df_module.BootstrapServerChangedEvent
+DatabaseCreatedEvent = df_module.DatabaseCreatedEvent
+DatabaseEndpointsChangedEvent = df_module.DatabaseEndpointsChangedEvent
+DatabaseRequires = df_module.DatabaseRequires
+DataRequires = df_module.DataRequires
+IndexCreatedEvent = df_module.IndexCreatedEvent
+KafkaRequires = df_module.KafkaRequires
+OpenSearchRequires = df_module.OpenSearchRequires
+TopicCreatedEvent = df_module.TopicCreatedEvent
+
+logger = logging.getLogger(__name__)
+
+# Extra roles that this application needs when interacting with the database.
+EXTRA_USER_ROLES = "SUPERUSER"
+EXTRA_USER_ROLES_KAFKA = "producer,consumer"
+EXTRA_USER_ROLES_OPENSEARCH = "admin,default"
+CONSUMER_GROUP_PREFIX = "test-prefix"
+
+
+class ApplicationCharm(CharmBase):
+    """Application charm that connects to database charms."""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        # Default charm events.
+        self.framework.observe(self.on.start, self._on_start)
+        self.framework.observe(self.on.get_plugin_status_action, self._on_get_plugin_status)
+
+        # Events related to the first database that is requested
+        # (these events are defined in the database requires charm library).
+        database_name = f'{self.app.name.replace("-", "_")}_first_database'
+        self.first_database = DatabaseRequires(
+            self, "first-database", database_name, EXTRA_USER_ROLES
+        )
+        self.framework.observe(
+            self.first_database.on.database_created, self._on_first_database_created
+        )
+        self.framework.observe(
+            self.first_database.on.endpoints_changed, self._on_first_database_endpoints_changed
+        )
+
+        # Events related to the second database that is requested
+        # (these events are defined in the database requires charm library).
+        database_name = f'{self.app.name.replace("-", "_")}_second_database'
+        self.second_database = DatabaseRequires(
+            self, "second-database", database_name, EXTRA_USER_ROLES
+        )
+        self.framework.observe(
+            self.second_database.on.database_created, self._on_second_database_created
+        )
+        self.framework.observe(
+            self.second_database.on.endpoints_changed, self._on_second_database_endpoints_changed
+        )
+
+        # Multiple database clusters charm events (clusters/relations without alias).
+        database_name = f'{self.app.name.replace("-", "_")}_multiple_database_clusters'
+        self.database_clusters = DatabaseRequires(
+            self, "multiple-database-clusters", database_name, EXTRA_USER_ROLES
+        )
+        self.framework.observe(
+            self.database_clusters.on.database_created, self._on_cluster_database_created
+        )
+        self.framework.observe(
+            self.database_clusters.on.endpoints_changed,
+            self._on_cluster_endpoints_changed,
+        )
+
+        # Multiple database clusters charm events (defined dynamically
+        # in the database requires charm library, using the provided cluster/relation aliases).
+        database_name = f'{self.app.name.replace("-", "_")}_aliased_multiple_database_clusters'
+        cluster_aliases = ["cluster1", "cluster2"]  # Aliases for the multiple clusters/relations.
+        self.aliased_database_clusters = DatabaseRequires(
+            self,
+            "aliased-multiple-database-clusters",
+            database_name,
+            EXTRA_USER_ROLES,
+            cluster_aliases,
+        )
+        # Each database cluster will have its own events
+        # with the name having the cluster/relation alias as the prefix.
+        self.framework.observe(
+            self.aliased_database_clusters.on.cluster1_database_created,
+            self._on_cluster1_database_created,
+        )
+        self.framework.observe(
+            self.aliased_database_clusters.on.cluster1_endpoints_changed,
+            self._on_cluster1_endpoints_changed,
+        )
+        self.framework.observe(
+            self.aliased_database_clusters.on.cluster2_database_created,
+            self._on_cluster2_database_created,
+        )
+        self.framework.observe(
+            self.aliased_database_clusters.on.cluster2_endpoints_changed,
+            self._on_cluster2_endpoints_changed,
+        )
+
+        # Kafka events
+
+        self.kafka = KafkaRequires(
+            self, "kafka-client", "test-topic", EXTRA_USER_ROLES_KAFKA, CONSUMER_GROUP_PREFIX
+        )
+
+        self.framework.observe(
+            self.kafka.on.bootstrap_server_changed, self._on_kafka_bootstrap_server_changed
+        )
+        self.framework.observe(self.kafka.on.topic_created, self._on_kafka_topic_created)
+
+        # OpenSearch events
+
+        self.opensearch = OpenSearchRequires(
+            self, "opensearch-client", "test-index", EXTRA_USER_ROLES_OPENSEARCH
+        )
+        self.framework.observe(self.opensearch.on.index_created, self._on_opensearch_index_created)
+
+        # actions
+
+        self.framework.observe(self.on.reset_unit_status_action, self._on_reset_unit_status)
+        self.has_secrets = JujuVersion.from_environ().has_secrets
+
+    def _on_start(self, _) -> None:
+        """Only sets an Active status."""
+        self.unit.status = ActiveStatus()
+
+    def _get_username_password(self, requires: DataRequires, event: EventBase):
+        if hasattr(event, "username"):
+            return event.username, event.password
+        else:
+            data = requires.get_relation_fields(event.relation.id, ["username", "password"])
+            return data.get("username"), data.get("password")
+
+    def _get_endpoints(self, requires: DataRequires, event: EventBase):
+        if hasattr(event, "username"):
+            return event.endpoints
+        else:
+            data = requires.get_relation_field(event.relation.id, "endpoints")
+            return data.get("endpoints")
+
+    def _get_bootstrap_server(self, requires: DataRequires, event: EventBase):
+        if hasattr(event, "username"):
+            return event.bootstrap_server
+        else:
+            data = requires.get_relation_field(event.relation.id, "endpoints")
+            return data.get("endpoints")
+
+    # First database events observers.
+    def _on_first_database_created(self, event: DatabaseCreatedEvent) -> None:
+        """Event triggered when a database was created for this application."""
+        # Retrieve the credentials using the charm library.
+        username, password = self._get_username_password(self.first_database, event)
+        logger.info(f"first database credentials: {username} {password}")
+        self.unit.status = ActiveStatus("received database credentials of the first database")
+
+    def _on_first_database_endpoints_changed(self, event: DatabaseEndpointsChangedEvent) -> None:
+        """Event triggered when the read/write endpoints of the database change."""
+        endpoints = self._get_endpoints(self.first_database, event)
+        logger.info(f"first database endpoints have been changed to: {endpoints}")
+
+    # Second database events observers.
+    def _on_second_database_created(self, event: DatabaseCreatedEvent) -> None:
+        """Event triggered when a database was created for this application."""
+        # Retrieve the credentials using the charm library.
+        username, password = self._get_username_password(self.second_database, event)
+        logger.info(f"second database credentials: {username} {password}")
+        self.unit.status = ActiveStatus("received database credentials of the second database")
+
+    def _on_second_database_endpoints_changed(self, event: DatabaseEndpointsChangedEvent) -> None:
+        """Event triggered when the read/write endpoints of the database change."""
+        endpoints = self._get_endpoints(event)
+        logger.info(f"second database endpoints have been changed to: {endpoints}")
+
+    # Multiple database clusters events observers.
+    def _on_cluster_database_created(self, event: DatabaseCreatedEvent) -> None:
+        """Event triggered when a database was created for this application."""
+        # Retrieve the credentials using the charm library.
+        username, password = self._get_username_password(self.database_clusters, event)
+        logger.info(
+            f"cluster {event.relation.app.name} credentials: {username} {password}"
+        )
+        self.unit.status = ActiveStatus(
+            f"received database credentials for cluster {event.relation.app.name}"
+        )
+
+    def _on_cluster_endpoints_changed(self, event: DatabaseEndpointsChangedEvent) -> None:
+        """Event triggered when the read/write endpoints of the database change."""
+        endpoints = self._get_endpoints(self.database_clusters, event)
+        logger.info(
+            f"cluster {event.relation.app.name} endpoints have been changed to: {endpoints}"
+        )
+
+    # Multiple database clusters events observers (for aliased clusters/relations).
+    def _on_cluster1_database_created(self, event: DatabaseCreatedEvent) -> None:
+        """Event triggered when a database was created for this application."""
+        # Retrieve the credentials using the charm library.
+        username, password = self._get_username_password(self.aliased_database_clusters, event)
+        logger.info(f"cluster1 credentials: {username} {password}")
+        self.unit.status = ActiveStatus("received database credentials for cluster1")
+
+    def _on_cluster1_endpoints_changed(self, event: DatabaseEndpointsChangedEvent) -> None:
+        """Event triggered when the read/write endpoints of the database change."""
+        endpoints = self._get_endpoints(self.aliased_database_clusters, event)
+        logger.info(f"cluster1 endpoints have been changed to: {endpoints}")
+
+    def _on_cluster2_database_created(self, event: DatabaseCreatedEvent) -> None:
+        """Event triggered when a database was created for this application."""
+        # Retrieve the credentials using the charm library.
+        username, password = self._get_username_password(self.aliased_database_clusters, event)
+        logger.info(f"cluster2 credentials: {username} {password}")
+        self.unit.status = ActiveStatus("received database credentials for cluster2")
+
+    def _on_cluster2_endpoints_changed(self, event: DatabaseEndpointsChangedEvent) -> None:
+        """Event triggered when the read/write endpoints of the database change."""
+        endpoints = self._get_endpoints(self.aliased_database_clusters, event)
+        logger.info(f"cluster2 endpoints have been changed to: {endpoints}")
+
+    def _on_get_plugin_status(self, event: ActionEvent) -> None:
+        """Returns the PostgreSQL plugin status (enabled/disabled)."""
+        plugin = event.params.get("plugin")
+        if not plugin:
+            event.fail("Please provide a plugin name")
+            return
+
+        plugin_status = (
+            "enabled" if self.first_database.is_postgresql_plugin_enabled(plugin) else "disabled"
+        )
+        event.set_results({"plugin-status": plugin_status})
+
+    def _on_kafka_bootstrap_server_changed(self, event: BootstrapServerChangedEvent):
+        """Event triggered when a bootstrap server was changed for this application."""
+        bootstrap_server = self._get_bootstrap_server(self.kafka, event)
+        logger.info(
+            f"On kafka boostrap-server changed: bootstrap-server: {bootstrap_server}"
+        )
+        self.unit.status = ActiveStatus("kafka_bootstrap_server_changed")
+
+    def _on_kafka_topic_created(self, _: TopicCreatedEvent):
+        """Event triggered when a topic was created for this application."""
+        logger.info("On kafka topic created")
+        self.unit.status = ActiveStatus("kafka_topic_created")
+
+    def _on_opensearch_index_created(self, _: IndexCreatedEvent):
+        """Event triggered when an index was created for this application."""
+        logger.info("On opensearch index created event fired")
+        self.unit.status = ActiveStatus("opensearch_index_created")
+
+    def _on_reset_unit_status(self, _: ActionEvent):
+        """Handle the reset of status message for the unit."""
+        self.unit.status = ActiveStatus()
+
+
+if __name__ == "__main__":
+    main(ApplicationCharm)
---
+++ tests/integration/charms/s3-charm/src/charm.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""S3 provider charm that accepts connections from application charms.
+
+This charm is meant to be used only for testing
+of the libraries in this repository.
+"""
+
+import logging
+
+from ops.charm import CharmBase
+from ops.main import main
+from ops.model import ActiveStatus, MaintenanceStatus
+
+from charms.data_platform_libs.v0.s3 import CredentialRequestedEvent, S3Provider
+
+logger = logging.getLogger(__name__)
+
+
+class S3Charm(CharmBase):
+    """S3 charm that accepts connections from application charms."""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        # Default charm events.
+        self.framework.observe(self.on.start, self._on_start)
+
+        # Charm events defined in the s3 provides charm library.
+        self.s3_provider = S3Provider(self, relation_name="s3-credentials")
+        self.framework.observe(
+            self.s3_provider.on.credentials_requested, self._on_credential_requested
+        )
+
+    def _on_start(self, _) -> None:
+        """Only sets an waiting status."""
+        # self.unit.status = WaitingStatus("Waiting for relation")
+        self.unit.status = ActiveStatus("HERE")
+
+    def _on_credential_requested(self, event: CredentialRequestedEvent):
+        self.unit.status = MaintenanceStatus("Creating credentials")
+        # retrieve bucket name from the requirer side
+        bucket = event.bucket
+
+        relation_id = event.relation.id
+
+        connection_info = {
+            "access-key": "test-access-key",
+            "secret-key": "test-secret-key",
+            "bucket": f"{bucket}",
+            "path": "/path/",
+            "endpoint": "s3.amazonaws.com",
+            "region": "us",
+            "s3-uri-style": "",
+            "storage-class": "cinder",
+            "tls-ca-chain": [],
+            "s3-api-version": "1.0",
+            "attributes": ["a1", "a2", "a3"],
+        }
+        # set connection info in the databag relation
+        self.s3_provider.update_connection_info(relation_id, connection_info)
+
+        self.unit.status = ActiveStatus()
+
+
+if __name__ == "__main__":
+    main(S3Charm)
