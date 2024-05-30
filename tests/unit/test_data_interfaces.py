234663f5a89db23afeab8485fd5dcd470897f673
Alex Batisse <alex.batisse@canonical.com>
2024-05-30 09:42:31 +0200

    tests: Add unit test for data interface cached secret

--- tests/unit/test_data_interfaces.py
+++ tests/unit/test_data_interfaces.py
@@ -20,6 +20,7 @@ from parameterized import parameterized
 from charms.data_platform_libs.v0.data_interfaces import (
     PROV_SECRET_PREFIX,
     REQ_SECRET_FIELDS,
+    CachedSecret,
     DatabaseCreatedEvent,
     DatabaseEndpointsChangedEvent,
     DatabaseProvides,
@@ -2777,3 +2778,38 @@ class TestOpenSearchRequires(DataRequirerBaseTests, unittest.TestCase):
         assert event.password == "test-password"
         assert event.endpoints == "host1:port,host2:port"
         assert event.tls_ca == "Canonical"
+
+
+class EmptyCharm(CharmBase):
+    """Mock database charm to use in units tests."""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+@pytest.fixture
+def harness() -> Harness:
+    harness = Harness(EmptyCharm)
+    harness.set_leader(True)
+    harness.begin_with_initial_hooks()
+    return harness
+
+
+@pytest.mark.usefixtures("only_with_juju_secrets")
+@pytest.mark.parametrize("scope", [("app"), ("unit")])
+def test_cached_secret_no_update(scope, harness: Harness[EmptyCharm], monkeypatch):
+    """Check that a cached secret do not trigger a revision if no update to content."""
+    # Given
+    content = {"value": "initialvalue"}
+    secret = CachedSecret(harness.model, getattr(harness.charm, scope), "my-label")
+    secret.add_secret(content)
+    patched = Mock()
+    # When
+    with monkeypatch.context() as m:
+        m.setattr(secret.meta, "set_content", patched)
+        secret.set_content(content)
+
+        secret.set_content({"value": "newvalue"})
+
+    # Then
+    patched.assert_called_once()
