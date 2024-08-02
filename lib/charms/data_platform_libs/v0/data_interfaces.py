bc15b15b39c771abc33a766b7e7338924103f22d
Judit Novak <judit.novak@canonical.com>
2024-08-02 14:06:39 +0200

    Increasing libpatch version

--- lib/charms/data_platform_libs/v0/data_interfaces.py
+++ lib/charms/data_platform_libs/v0/data_interfaces.py
@@ -331,7 +331,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 38
+LIBPATCH = 39
 
 PYDEPS = ["ops>=2.0.0"]
 
