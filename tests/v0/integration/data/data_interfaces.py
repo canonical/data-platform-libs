b259b0504c0108e155043ced34efb597d7c175c7
MattiaSarti <nmattia1@gmail.com>
2025-06-10 09:43:26 +0200

    build: bump version patch

--- tests/v0/integration/data/data_interfaces.py
+++ tests/v0/integration/data/data_interfaces.py
@@ -316,7 +316,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 47
+LIBPATCH = 48
 
 PYDEPS = ["ops>=2.0.0"]
 
