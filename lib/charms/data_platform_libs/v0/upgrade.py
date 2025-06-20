360a08039a0c6d76f295e3d840c9cfcb2d395cd9
Iman Enami <44609233+imanenami@users.noreply.github.com>
2025-06-20 13:07:20 +0400

    fix: bump changed libs LIBPATCH (#228)

--- lib/charms/data_platform_libs/v0/upgrade.py
+++ lib/charms/data_platform_libs/v0/upgrade.py
@@ -285,7 +285,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 18
+LIBPATCH = 19
 
 PYDEPS = ["pydantic>=1.10,<2", "poetry-core"]
 
