dae7f2da717a1800e4077e9f8337d65052a64391
MattiaSarti <nmattia1@gmail.com>
2025-07-03 14:45:51 +0200

    other: bump version

--- lib/charms/data_platform_libs/v0/data_interfaces.py
+++ lib/charms/data_platform_libs/v0/data_interfaces.py
@@ -331,7 +331,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 48
+LIBPATCH = 49
 
 PYDEPS = ["ops>=2.0.0"]
 
